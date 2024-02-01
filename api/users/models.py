addc018558f2ba6989cf27f8798b5cb0372a3ec3
dgolov <dmitriy.evgenyevich@gmail.com>
2024-02-01 13:48:30 +0300

    added models

--- api/users/models.py
+++ api/users/models.py
@@ -16,7 +16,7 @@ class User(SQLAlchemyBaseUserTable[int], Base):
     is_active: bool = Column(Boolean, default=True, nullable=False)
     is_superuser: bool = Column(Boolean, default=False, nullable=False)
     is_verified: bool = Column(Boolean, default=False, nullable=False)
-    players = relationship("Player", lazy="selectin", join_depth=2)
+    programs = relationship("Program", lazy="selectin", join_depth=2)
 
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
